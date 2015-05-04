f7f111b3d49e7bd1717dd475344ab3d10143c0cb
Marijn Brosens <marijnbrosens16@hotmail.com>
2015-05-04 15:52:18 +0200

    array

--- project/Quiz/src/hellotvxlet/HelloTVXlet.java
+++ project/Quiz/src/hellotvxlet/HelloTVXlet.java
@@ -1,10 +1,10 @@
-
 package hellotvxlet;
 
 import javax.tv.xlet.*;
 import org.dvb.ui.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Arrays;
 
 import org.havi.ui.*;
 import org.havi.ui.event.*;
@@ -15,19 +15,28 @@ public class HelloTVXlet implements Xlet, UserEventListener, HActionListener {
     
     private XletContext actueleXletContext;
     private HScene scene;
-    private HStaticText title, question, answerLable, scoreLable;
-    private HTextButton knop1, knop2, knop3, knop4, knop5, knop6, knop7, knop8;
-    private String isTrue = "";
+    HelloTVXlet xlet;
     //debuggen activeren of niet?
-    private boolean debug = true;
-    public int score = 0;
+    private boolean debug = true;    
+    private HStaticText title, vraag, antwoordLabel;    
+    private HTextButton knop1, knop2, knop3, knop4, knopHulp;    
+    private String isJuist = "";    
     
+    String[][] questions = { 
+            {"vraag1", "antwoord11", "antwoord12","antwoord13","antwoord14","1"},
+            {"vraag2", "antwoord21", "antwoord22","antwoord23","antwoord24","2"},
+            {"vraag3", "antwoord31", "antwoord32","antwoord32","antwoord34","3"},
+            {"vraag4", "antwoord41", "antwoord42","antwoord43","antwoord44","4"}
+        };   
+    
+    private int randomvraagIndex = 0 + (int)(Math.random()*questions.length); 
+
     public HelloTVXlet() {
         
     }
-    
+
     public void initXlet(XletContext context) throws XletStateChangeException {
-        
+      
         if(debug) System.out.println("Xlet Initialiseren");
         
         this.actueleXletContext = context;
@@ -35,95 +44,79 @@ public class HelloTVXlet implements Xlet, UserEventListener, HActionListener {
         HSceneTemplate sceneTemplate = new HSceneTemplate();
         
         sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_DIMENSION,
-                                    new HScreenDimension(1.0f, 1.0f),   //Procentueel
-                                    HSceneTemplate.REQUIRED);
+                new HScreenDimension(1.0f, 1.0f),   //Procentueel
+                HSceneTemplate.REQUIRED);
         
         sceneTemplate.setPreference(HSceneTemplate.SCENE_SCREEN_LOCATION,
-                                    new HScreenPoint(0.0f, 0.0f),
-                                    HSceneTemplate.REQUIRED);
+                new HScreenPoint(0.0f, 0.0f),
+                HSceneTemplate.REQUIRED);
         
         scene = HSceneFactory.getInstance().getBestScene(sceneTemplate);
         
-        title = new HStaticText("Logo quiz");
-        title.setLocation(0,0);
+        title = new HStaticText("Wie wordt multimiljonair?");        
+        title.setLocation(0,0); //in pixels
         title.setSize(740,100);
         title.setBackground(new DVBColor(0,0,0, 100));
         title.setBackgroundMode(HVisible.BACKGROUND_FILL);
         
-        question = new HStaticText("Van welk merk is het logo?");
-        question.setLocation(0,100);
-        question.setSize(740,60);
-        question.setBackground(new DVBColor(0,0,0, 150));
-        question.setBackgroundMode(HVisible.BACKGROUND_FILL);
         
-        answerLable = new HStaticText(isTrue);
-        answerLable.setLocation(0,500);
-        answerLable.setSize(740,80);
-        answerLable.setBackground(new DVBColor(0,0,0, 150));
-        answerLable.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        int i=0;
+        while( i< questions.length)
+        {            
+            vraag = new HStaticText(questions[randomvraagIndex][0]);   
+            
+            knop1 = new HTextButton(questions[randomvraagIndex][1]); 
+            knop2 = new HTextButton(questions[randomvraagIndex][2]);  
+            knop3 = new HTextButton(questions[randomvraagIndex][3]);  
+            knop4 = new HTextButton(questions[randomvraagIndex][4]);         
+    
+            i++;
+        } 
         
-        scoreLable = new HStaticText(Integer.toString(score));
-        scoreLable.setLocation(0,40);
-        scoreLable.setSize(740,100);
-        //scoreLable.setBackground(new DVBColor(0,0,0, 100));
-        scoreLable.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        
+        //vraag = new HStaticText("Hoe maak je rood in RGB?");        
+        vraag.setLocation(0,100);
+        vraag.setSize(740,60);
+        vraag.setBackground(new DVBColor(0,0,0, 150));
+        vraag.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        
+        antwoordLabel = new HStaticText(isJuist);        
+        antwoordLabel.setLocation(0,500);
+        antwoordLabel.setSize(740,80);
+        antwoordLabel.setBackground(new DVBColor(0,0,0, 150));
+        antwoordLabel.setBackgroundMode(HVisible.BACKGROUND_FILL);
         
         scene.add(title);
-        scene.add(question);
-        
-        knop1 = new HTextButton("BMW");
+        scene.add(vraag);
+                
         knop1.setLocation(220,180); //in pixels
         knop1.setSize(300,50);
         knop1.setBackground(new DVBColor(100,100,100, 179));
         knop1.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        
-        knop2 = new HTextButton("Mercedes");
+              
         knop2.setLocation(220,240); //in pixels
         knop2.setSize(300,50);
         knop2.setBackground(new DVBColor(100,100,100, 179));
         knop2.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        
-        knop3 = new HTextButton("Peugeot");
+             
         knop3.setLocation(220,300); //in pixels
         knop3.setSize(300,50);
         knop3.setBackground(new DVBColor(100,100,100, 179));
         knop3.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        
-        knop4 = new HTextButton("Volvo");
+             
         knop4.setLocation(220,360); //in pixels
         knop4.setSize(300,50);
         knop4.setBackground(new DVBColor(100,100,100, 179));
         knop4.setBackgroundMode(HVisible.BACKGROUND_FILL);
-      
-        knop5 = new HTextButton("Volkswagen");
-        knop5.setLocation(220,180); //in pixels
-        knop5.setSize(300,50);
-        knop5.setBackground(new DVBColor(100,100,100, 179));
-        knop5.setBackgroundMode(HVisible.BACKGROUND_FILL);
         
-        knop6 = new HTextButton("Audi");
-        knop6.setLocation(220,240); //in pixels
-        knop6.setSize(300,50);
-        knop6.setBackground(new DVBColor(100,100,100, 179));
-        knop6.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        
-        knop7 = new HTextButton("Renault");
-        knop7.setLocation(220,300); //in pixels
-        knop7.setSize(300,50);
-        knop7.setBackground(new DVBColor(100,100,100, 179));
-        knop7.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        
-        knop8 = new HTextButton("CitroÎn");
-        knop8.setLocation(220,360); //in pixels
-        knop8.setSize(300,50);
-        knop8.setBackground(new DVBColor(100,100,100, 179));
-        knop8.setBackgroundMode(HVisible.BACKGROUND_FILL);
+
         
         //Interactiviteit implementeren
         knop1.setFocusTraversal(knop4, knop2, null, null);//op, neer, links, rechts
         knop2.setFocusTraversal(knop1, knop3, null, null);//op, neer, links, rechts
         knop3.setFocusTraversal(knop2, knop4, null, null);//op, neer, links, rechts
         knop4.setFocusTraversal(knop3, knop1, null, null);//op, neer, links, rechts
+//        knopHulp.setFocusTraversal(knop2, null, null, null);
         
         knop1.setActionCommand("1");
         knop1.addHActionListener(this);
@@ -136,99 +129,79 @@ public class HelloTVXlet implements Xlet, UserEventListener, HActionListener {
         
         knop4.setActionCommand("4");
         knop4.addHActionListener(this);
+//        knopHulp.setActionCommand("Help");
+//        knopHulp.addHActionListener(this);
 
         scene.add(knop1);
         scene.add(knop2);
         scene.add(knop3);
         scene.add(knop4);
-        scene.add(answerLable);
-        scene.add(scoreLable);
+//        scene.add(knopHulp);
+        scene.add(antwoordLabel);
         
-        knop1.requestFocus();
-        
+        knop1.requestFocus();     
     }
-    
+
     public void startXlet() {
+    
         scene.validate();
         scene.setVisible(true);
     }
-    
+
     public void pauseXlet() {
-        
+     
     }
-    
+
     public void destroyXlet(boolean unconditional) {
         
     }
-    
+
     public void userEventReceived(UserEvent e) {
         
     }
-    
+
     public void actionPerformed(ActionEvent e) throws UnsupportedOperationException {
-        
         handleClick(e.getActionCommand());
-        
     }
     
     private void handleClick(String command){
         
-        if(command =="1"){
+        int i=0;
+        while( i< questions.length)
+        {            
+            vraag = new HStaticText(questions[randomvraagIndex][0]);   
             
-            isTrue = "False";
-            knop1.setBackground(new DVBColor(255,0,0, 179));
-            knop1.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            
-        }else if(command == "2"){
-            
-            isTrue = "False";
-            knop2.setBackground(new DVBColor(255,0,0, 179));
-            knop2.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            
-        }else if(command == "3"){
-            
-            isTrue = "Right";
-            knop3.setBackground(new DVBColor(0,255,0, 179));
-            knop3.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            score+=100;
-            
-        }else if(command == "4"){
-            
-            isTrue = "False";
-            knop4.setBackground(new DVBColor(255,0,0, 179));
-            knop4.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            
-        }else if(command == "5"){
-            
-            isTrue = "False";
-            knop5.setBackground(new DVBColor(255,0,0, 179));
-            knop5.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            
-        }else if(command == "6"){
-            
-            isTrue = "False";
-            knop6.setBackground(new DVBColor(255,0,0, 179));
-            knop6.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            
-        }else if(command == "7"){
-            
-            isTrue = "False";
-            knop7.setBackground(new DVBColor(255,0,0, 179));
-            knop7.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            
-        }else if(command == "8"){
-            
-            isTrue = "False";
-            knop8.setBackground(new DVBColor(255,0,0, 179));
-            knop8.setBackgroundMode(HVisible.BACKGROUND_FILL);
-            
-        }else{
-            
-            isTrue = "False";
-            
-        }
+            knop1 = new HTextButton(questions[randomvraagIndex][1]); 
+            knop2 = new HTextButton(questions[randomvraagIndex][2]);  
+            knop3 = new HTextButton(questions[randomvraagIndex][3]);  
+            knop4 = new HTextButton(questions[randomvraagIndex][4]);         
+    
+            i++;
+        }  
         
-        this.answerLable.setTextContent(isTrue, this.answerLable.NORMAL_STATE);
-        this.scoreLable.setTextContent(Integer.toString(score), this.scoreLable.NORMAL_STATE);
+        if(command == questions[randomvraagIndex][5]){
+            
+            isJuist = "Juist";
+            randomvraagIndex = 0 + (int)(Math.random()*questions.length); 
+            this.xlet = xlet;
+            //knop1.setBackground(new DVBColor(255,0,0, 179));
+            //knop1.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        }else{
+            isJuist = "fout";
+            //knop2.setBackground(new DVBColor(255,0,0, 179));
+            //knop2.setBackgroundMode(HVisible.BACKGROUND_FILL); 
+        }
+        /*else if(command == "Help"){
+            getHelp();
+        }*/
+        
+        this.antwoordLabel.setTextContent(isJuist, this.antwoordLabel.NORMAL_STATE);
     }
+    
+/*    private void getHelp(){
+        knop1.setVisible(false);
+        knop4.setVisible(false);
+        knop2.setFocusTraversal(knop3, knopHulp, null, knop3);
+        knop3.setFocusTraversal(null, knop2, knop2, null);
+    }*/
 }
