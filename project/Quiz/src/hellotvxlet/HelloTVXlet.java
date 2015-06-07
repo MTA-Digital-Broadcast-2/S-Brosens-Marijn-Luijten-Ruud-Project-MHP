0498bd8da446f0d38206566739d92563978d6e1f
Marijn Brosens <marijnbrosens16@hotmail.com>
2015-06-07 04:47:37 +0200

    images added , start, stop, score niet onder 0

--- project/Quiz/src/hellotvxlet/HelloTVXlet.java
+++ project/Quiz/src/hellotvxlet/HelloTVXlet.java
@@ -1,11 +1,13 @@
 package hellotvxlet;
 
-import java.awt.event.KeyEvent;
 import javax.tv.xlet.*;
 import org.dvb.ui.*;
+
+import java.util.Arrays;
+
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Arrays;
+import java.awt.event.KeyEvent;
 
 import org.havi.ui.*;
 import org.havi.ui.event.*;
@@ -13,14 +15,16 @@ import org.dvb.event.*;
 
 public class HelloTVXlet implements Xlet, UserEventListener, HActionListener 
 {
+    HelloTVXlet xlet;
+    
     private XletContext actueleXletContext;
     private HScene scene;
-    HelloTVXlet xlet;
     private boolean debug = true;    
     private HStaticText title, vraag, antwoordLabel, scoreLabel;    
-    private HTextButton knop1, knop2, knop3, knop4, knopHulp;    
+    private HTextButton startknop, stopknop, knop1, knop2, knop3, knop4;    
     private String isJuist = "";    
     private int score = 0;
+    private DrawImage background;
     
     private String[][] questions = 
     { 
@@ -40,7 +44,7 @@ public class HelloTVXlet implements Xlet, UserEventListener, HActionListener
         {"15.Welk dier bestaat niet?", "A.Heldenbok", "B.Vliegend hert","C.Franjestaart","D.Ankerhoorn","4"},
         {"16.Welke plant bestaat niet?", "A.Bitterzoet", "B.Grote wederik","C.Plaatjesaar","D.Speenkruid","3"}
     };   
-    
+        
     private int randomvraagIndex = 0 + ( int )( Math.random()*questions.length ); 
     
     public HelloTVXlet() {}
@@ -53,13 +57,7 @@ public class HelloTVXlet implements Xlet, UserEventListener, HActionListener
         sceneTemplate.setPreference( HSceneTemplate.SCENE_SCREEN_LOCATION, new HScreenPoint( 0.0f, 0.0f ), HSceneTemplate.REQUIRED );
         
         scene = HSceneFactory.getInstance().getBestScene( sceneTemplate );
-        
-        title = new HStaticText( "Natuurvragen." );        
-        title.setLocation( 0,0 ); //in pixels
-        title.setSize( 740,100 );
-        title.setBackground( new DVBColor( 0,0,0, 100 ) );
-        title.setBackgroundMode( HVisible.BACKGROUND_FILL );       
-        
+  
         int i=0;
         while( i< questions.length)
         {            
@@ -71,6 +69,12 @@ public class HelloTVXlet implements Xlet, UserEventListener, HActionListener
     
             i++;
         } 
+        
+        title = new HStaticText( "Natuurvragen." );        
+        title.setLocation( 0,0 );
+        title.setSize( 740,100 );
+        title.setBackground( new DVBColor( 0,0,0, 150 ) );
+        title.setBackgroundMode( HVisible.BACKGROUND_FILL ); 
                
         vraag.setLocation( 0,100 );
         vraag.setSize( 740,60 );
@@ -84,37 +88,55 @@ public class HelloTVXlet implements Xlet, UserEventListener, HActionListener
         antwoordLabel.setBackgroundMode( HVisible.BACKGROUND_FILL );
         
         scoreLabel = new HStaticText( Integer.toString( score ) );        
-        scoreLabel.setLocation( 0,400 );
-        scoreLabel.setSize( 740,80 );
+        scoreLabel.setLocation( 30,250 );
+        scoreLabel.setSize( 100,100 );
         
         scene.add( title );
         scene.add( vraag );
-                
-        knop1.setLocation( 220,180 ); //in pixels
-        knop1.setSize( 300,50 );
+        
+        startknop = new HTextButton( "Start." );        
+        startknop.setLocation( 170,250 );
+        startknop.setSize( 400,100 );
+        startknop.setBackground( new DVBColor( 0,0,0, 200 ) );
+        startknop.setBackgroundMode( HVisible.BACKGROUND_FILL );
+        
+        stopknop = new HTextButton( "Stop." );        
+        stopknop.setLocation( 30,180 ); 
+        stopknop.setSize( 100,100 );
+        stopknop.setBackground( new DVBColor( 0,0,0, 200 ) );
+        stopknop.setBackgroundMode( HVisible.BACKGROUND_FILL );
+       
+        knop1.setLocation( 170,180 ); 
+        knop1.setSize( 400,70 );
         knop1.setBackground( new DVBColor( 100,100,100, 179 ) );
         knop1.setBackgroundMode( HVisible.BACKGROUND_FILL );
               
-        knop2.setLocation( 220,240 ); //in pixels
-        knop2.setSize( 300,50 );
+        knop2.setLocation( 170,250 ); 
+        knop2.setSize( 400,70 );
         knop2.setBackground( new DVBColor( 100,100,100, 179 ) );
         knop2.setBackgroundMode( HVisible.BACKGROUND_FILL );
              
-        knop3.setLocation( 220,300 ); //in pixels
-        knop3.setSize( 300,50 );
+        knop3.setLocation( 170,320 ); 
+        knop3.setSize( 400,70 );
         knop3.setBackground( new DVBColor( 100,100,100, 179 ) );
         knop3.setBackgroundMode( HVisible.BACKGROUND_FILL );
              
-        knop4.setLocation( 220,360 ); //in pixels
-        knop4.setSize( 300,50 );
+        knop4.setLocation( 170,390 ); 
+        knop4.setSize( 400,70 );
         knop4.setBackground( new DVBColor( 100,100,100, 179 ) );
         knop4.setBackgroundMode( HVisible.BACKGROUND_FILL );
-
-        knop1.setFocusTraversal( knop4, knop2, null, null );//op, neer, links, rechts
-        knop2.setFocusTraversal( knop1, knop3, null, null );//op, neer, links, rechts
-        knop3.setFocusTraversal( knop2, knop4, null, null );//op, neer, links, rechts
-        knop4.setFocusTraversal( knop3, knop1, null, null );//op, neer, links, rechts
-//      knopHulp.setFocusTraversal(knop2, null, null, null);
+        
+        knop1.setFocusTraversal( knop4, knop2, stopknop, stopknop );//op, neer, links, rechts
+        knop2.setFocusTraversal( knop1, knop3, stopknop, stopknop );//op, neer, links, rechts
+        knop3.setFocusTraversal( knop2, knop4, stopknop, stopknop );//op, neer, links, rechts
+        knop4.setFocusTraversal( knop3, knop1, stopknop, stopknop );//op, neer, links, rechts
+        stopknop.setFocusTraversal( null, null, knop1, knop4 );//op, neer, links, rechts
+        
+        startknop.setActionCommand( "play" );
+        startknop.addHActionListener( this );
+        
+        stopknop.setActionCommand( "stop" );
+        stopknop.addHActionListener( this );
         
         knop1.setActionCommand( "1" );
         knop1.addHActionListener( this );
@@ -127,47 +149,104 @@ public class HelloTVXlet implements Xlet, UserEventListener, HActionListener
         
         knop4.setActionCommand( "4" );
         knop4.addHActionListener( this );
-//      knopHulp.setActionCommand("Help");
-//      knopHulp.addHActionListener(this);
-
+        
+        background = new DrawImage("bg.png", 0, -35);
+        
+        scene.add( startknop );
+        scene.add( stopknop );
         scene.add( knop1 );
         scene.add( knop2 );
         scene.add( knop3 );
         scene.add( knop4 );
         scene.add( antwoordLabel );
         scene.add( scoreLabel );
-//      scene.add(knopHulp);
+        scene.add( background );
         
-        knop1.requestFocus();     
+        startknop.requestFocus();  
     }
 
-    public void startXlet() { scene.validate(); scene.setVisible(true); }
-    public void pauseXlet() {}
-    public void destroyXlet(boolean unconditional) throws XletStateChangeException {}
-    public void userEventReceived( UserEvent e ) {}
-    public void actionPerformed( ActionEvent e ) throws UnsupportedOperationException { handleClick(e.getActionCommand()); }    
+    public void startXlet() 
+    { 
+        scoreLabel.setVisible(false);
+        
+        scene.validate(); 
+        scene.setVisible(true); 
+        showHome();
+    }
     
-    private void handleClick(String command)
-    {              
+    public void pauseXlet() {}
+    public void destroyXlet( boolean unconditional ) throws XletStateChangeException {}
+    public void userEventReceived( UserEvent e ) {}
+    public void actionPerformed( ActionEvent e ) throws UnsupportedOperationException { handleClick( e.getActionCommand() ); }    
+    
+    public void showHome()
+    {      
+        antwoordLabel.setVisible( false );
+        vraag.setVisible( false );
+        
+        startknop.setVisible( true );
+        knop1.setVisible( false );
+        knop2.setVisible( false );
+        knop3.setVisible( false );
+        knop4.setVisible( false );
+        stopknop.setVisible( false );
+        
+        startknop.requestFocus();  
+    }
+    
+    public void showGame()
+    {
+        score = 0;
+        isJuist = "";
+        antwoordLabel.setBackground( new DVBColor( 0,0,0, 150 ) );
+        
+        antwoordLabel.setVisible( true );
+        scoreLabel.setVisible( true );
+        vraag.setVisible( true );
+
+        startknop.setVisible( false );
+        knop1.setVisible( true );
+        knop2.setVisible( true );
+        knop3.setVisible( true );
+        knop4.setVisible( true );
+        stopknop.setVisible( true );
+
+        knop1.requestFocus();  
+    }
+    
+    private void handleClick( String command )
+    {             
         if( command == questions[randomvraagIndex][5] )
         {            
             isJuist = "Juist";
-            score += 2;
+            score += 5;
             this.xlet = xlet;
             System.out.println( randomvraagIndex );
 
             randomvraagIndex = 0 + ( int )( Math.random() * questions.length ); 
-            System.out.println(questions[randomvraagIndex][0]);
-            antwoordLabel.setBackground(new DVBColor(0,255,0, 150));        
-        }else{            
-            score --;
+            System.out.println( questions[randomvraagIndex][0] );
+            antwoordLabel.setBackground( new DVBColor( 0,255,0, 150 ) );    
+            
+        } else if( command == "stop" ){
+            
+            showHome();
+            
+        } else if( command == "play" ){
+            
+            showGame();
+            
+        } else {      
+            
+            if( score > 2 ){
+                score -= 3;
+            } else {
+                score = 0; 
+            }
+            
             isJuist = "fout";
-            antwoordLabel.setBackground(new DVBColor(255,0,0, 150));           
+            antwoordLabel.setBackground(new DVBColor( 255,0,0, 150 ) );    
+            
         }
-        
-        /*else if(command == "Help"){
-            getHelp();
-        }*/
      
         vraag.setTextContent( questions[randomvraagIndex][0], this.vraag.NORMAL_STATE );        
         knop1.setTextContent( questions[randomvraagIndex][1], this.knop1.NORMAL_STATE );
@@ -175,14 +254,7 @@ public class HelloTVXlet implements Xlet, UserEventListener, HActionListener
         knop3.setTextContent( questions[randomvraagIndex][3], this.knop3.NORMAL_STATE );
         knop4.setTextContent( questions[randomvraagIndex][4], this.knop4.NORMAL_STATE );      
         
-        this.antwoordLabel.setTextContent(isJuist, this.antwoordLabel.NORMAL_STATE);
-        this.scoreLabel.setTextContent(Integer.toString(score), this.scoreLabel.NORMAL_STATE);
+        this.antwoordLabel.setTextContent( isJuist, this.antwoordLabel.NORMAL_STATE );
+        this.scoreLabel.setTextContent( "Score: " + Integer.toString( score ), this.scoreLabel.NORMAL_STATE );
     }
-    
-/*    private void getHelp(){
-        knop1.setVisible(false);
-        knop4.setVisible(false);
-        knop2.setFocusTraversal(knop3, knopHulp, null, knop3);
-        knop3.setFocusTraversal(null, knop2, knop2, null);
-    }*/
 }
